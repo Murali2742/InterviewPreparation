49a9d02bc3e93eb622dc87c56b8fa694c462128a
Murali Krishnan V <vmkrishnan2742@gmail.com>
2022-04-23 16:08:52 +0530

    second commit

---
+++ Arrays/reversearray.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+import java.util.Arrays;
+
+import java.util.Collections;
+
+public class reversearray {
+    
+    public static void main(String[] args) {
+
+
+
+        
+        Integer arr[] ={1,2,3};
+        //using Collections
+        Collections.reverse(Arrays.asList(arr));
+        System.out.print(Arrays.asList(arr));
+        
+
+    }
+}
